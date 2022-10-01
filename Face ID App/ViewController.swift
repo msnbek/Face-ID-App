7f191d3650619c98949735415e2acdc36319b58c
Msnbek <mahmutsenbek@icloud.com>
2022-10-01 15:44:09 +0300

    Initial Commit

---
+++ Face ID App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Face ID App
+//
+//  Created by Mahmut Şenbek on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
