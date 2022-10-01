31f7760277fbbaa8ff6a4d0af04ca16235f9e340
Msnbek <mahmutsenbek@icloud.com>
2022-10-01 16:04:46 +0300

    lastest version

--- Face ID App/ViewController.swift
+++ Face ID App/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickedButton(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Face ID testing") { success, error in
+                if success == true {
+                    
+                    DispatchQueue.main.sync {
+                        print("success")
+                        self.labelText.text = "success"
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                }else {
+                    DispatchQueue.main.sync {
+                        self.labelText.text = "Error!"
+                    }
+                }
+            }
+        }
+            
+    }
+    
 }
 
